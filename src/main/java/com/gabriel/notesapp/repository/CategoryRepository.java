610d8f9ba1c9271bd58284b2169cb005b07560c0
Gabriel Moura <77291941+Gabriel-sy@users.noreply.github.com>
2024-07-05 20:09:18 -0300

    feat: Update docker compose e add .env

--- src/main/java/com/gabriel/notesapp/repository/CategoryRepository.java
+++ src/main/java/com/gabriel/notesapp/repository/CategoryRepository.java
@@ -1,12 +1,14 @@
 package com.gabriel.notesapp.repository;
 
 import com.gabriel.notesapp.domain.category.Category;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
+@Transactional
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Optional<Category> findByCategoryName(String categoryName);
 }
--- src/main/java/com/gabriel/notesapp/repository/UserRepository.java
+++ src/main/java/com/gabriel/notesapp/repository/UserRepository.java
@@ -1,13 +1,14 @@
 package com.gabriel.notesapp.repository;
 
 import com.gabriel.notesapp.domain.user.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Transactional
 public interface UserRepository extends JpaRepository<User, Long> {
     UserDetails findByLogin(String login);
-    User findByPassword(String password);
 
 }
